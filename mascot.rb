932ebc0243542eecbeab6d0fe11b6f3b92175cb7
David Skaggs & klorealf <pair+davidtskaggs.klorealf@devbootcamp.com>
2017-08-28 19:26:34 -0400

    Refactor methods

--- mascot.rb
+++ mascot.rb
@@ -1,8 +1,9 @@
 # Get input from the user:  the cheer
 def call_out_cheer
-puts "Give me a cheer!"
-input = gets.chomp
-input
+  gets.chomp
+# puts "Give me a cheer!"
+# input = gets.chomp
+# input
 end
 
 # Determine the mascot's response based on the argument
@@ -10,27 +11,38 @@ end
 def mascot_sign_for(input)
   # call_out_cheer
   if input == "RED HOT"
-    response = "H-O-T!"
+     "H-O-T!"
   elsif input == "DO IT AGAIN"
-    response = "Go, Fight, Win"
+    "Go, Fight, Win"
   elsif input == "2 BITS"
-    response = "Holler!"
+     "Holler!"
   elsif input == "STOMP YOUR FEET"
-    response = "STOMP!"
+    "STOMP!"
   else
-    response = "Go Team!"
+     "Go Team!"
   end
-  response
+
 end
 
 # Print the argument passed to the method
 def display(response)
-  puts response
+  puts mascot_sign_for(response)
 end
 
 # This method will control the flow of the application,
 # making use of the other three methods.
 def coordinate_cheers
+  puts "What cheer do you want?"
+  cheer = call_out_cheer
+  counter = 0
+  until cheer == "GAME OVER" || counter == 2
+    if cheer == ""
+      counter += 1
+      puts "What cheer do you want?"
+      cheer = call_out_cheer
+    else
+      display(cheer)
+      cheer = call_out_cheer
+    end
+  end
 end
-
-# display(mascot_sign_for(call_out_cheer))
