6cc906a732ddcfb75b98aad54d7b30ed3c35855a
Amber Maillard & Andreas Bach Landgrebe <pair+amber2394.grebes15@devbootcamp.com>
2017-08-28 17:31:13 -0400

    finished release 0. Add rules and methods so mascot can show the proper sign for a specified cheer

--- mascot.rb
+++ mascot.rb
@@ -1,17 +1,40 @@
 # Get input from the user:  the cheer
 def call_out_cheer
+  puts "What would you like to cheer?"
+  response = gets.chomp
+  return response
 end
 
 # Determine the mascot's response based on the argument
 # passed to the method
 def mascot_sign_for(input)
+  if input == "RED HOT"
+    "H-O-T!"
+  elsif input == "DO IT AGAIN"
+    "Go, Fight, Win"
+  elsif input == "2 BITS"
+    "Holler!"
+  elsif input == "STOMP YOUR FEET"
+    "STOMP!"
+  else
+    "Go Team!"
+  end
 end
 
 # Print the argument passed to the method
 def display(response)
+  puts mascot_sign_for(response)
 end
 
 # This method will control the flow of the application,
 # making use of the other three methods.
 def coordinate_cheers
 end
+
+
+
+#puts mascot_sign_for("RED HOT")
+#puts mascot_sign_for("DO IT AGAIN")
+#puts mascot_sign_for("2 BITS")
+#puts mascot_sign_for("STOMP YOUR FEET")
+#puts mascot_sign_for("red hot")
