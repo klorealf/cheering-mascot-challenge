605a852678557a03c8fe4ba00d08a76f21d494cb
Samory Harris &   "name": null <pair+samory070.thierrymoudiki@devbootcamp.com>
2017-08-28 17:22:04 -0400

    Add release 0

--- mascot.rb
+++ mascot.rb
@@ -1,17 +1,38 @@
 # Get input from the user:  the cheer
 def call_out_cheer
+  puts "Specify a cheer!"
+ user_input = gets.chomp
+ return user_input
 end
 
 # Determine the mascot's response based on the argument
 # passed to the method
+
 def mascot_sign_for(input)
+  case input
+  when 'RED HOT'
+    p 'H-O-T!'
+  when 'DO IT AGAIN'
+    p 'Go, Fight, Win'
+  when '2 BITS'
+    p 'Holler!'
+  when 'STOMP YOUR FEET'
+    p 'STOMP!'
+  else
+    p 'Go Team!'
+  end
+
 end
+# mascot_sign_for('STOMP YOUR FEET')
 
 # Print the argument passed to the method
 def display(response)
+  puts response
 end
 
 # This method will control the flow of the application,
 # making use of the other three methods.
 def coordinate_cheers
 end
+
+
