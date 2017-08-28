38d3f5f9cf49580f3cd595318e422edd70d1eb33
Chris Bordes & Chris Camp <pair+cbordes8.cpcamp2@devbootcamp.com>
2017-08-28 17:41:24 -0400

    Add code for Release 1

--- mascot.rb
+++ mascot.rb
@@ -1,32 +1,40 @@
-# Get input from the user:  the cheer
 def call_out_cheer
 puts "Whats your CHEER!!?!?!"
 input = gets.chomp
-return input
+
 end
 
 # Determine the mascot's response based on the argument
 # passed to the method
 def mascot_sign_for(input)
   if input == "RED HOT"
-    response = "H-O-T!"
+    mascot_response = "H-O-T!"
   elsif input == "DO IT AGAIN"
-    response = "Go, Fight, Win"
+    mascot_response = "Go, Fight, Win"
   elsif input == "2 BITS"
-    response = "Holler!"
+    mascot_response = "Holler!"
   elsif input == "STOMP YOUR FEET"
-    response = "STOMP!"
-  else response = "Go Team!"
+    mascot_response = "STOMP!"
+  else mascot_response = "Go Team!"
   end
+  return mascot_response
 end
 
 # Print the argument passed to the method
-def display(response)
-  puts "Mascot holds up sign that says: #{response}"
+def display(mascot_response)
+  puts "Mascot holds up sign that says #{mascot_response}"
 end
 
 # This method will control the flow of the application,
 # making use of the other three methods.
 def coordinate_cheers
-
+  input_counter = 0
+  input = ""
+  until input == "GAME OVER"
+    input = call_out_cheer
+    mascot_response = mascot_sign_for(input)
+    display(mascot_response)
+  end
 end
+
+coordinate_cheers
